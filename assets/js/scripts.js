1c891d12123a15edb4d6c96234921e42dff31cf1
Steff Yang <wonderwallfrank@gmail.com>
2017-07-06 22:15:42 +0800

    contents

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,15 @@
 var data = {
     current_step: 1,
+    chart: {
+        type: 'bar',
+        x_label: '',
+        x_data: '',
+        y_label_1: '',
+        y_label_1_percentage: true,
+        y_label_2: '',
+        y_label_2_percentage: false,
+        y_dataset: [],
+    }
 };
 
 var vm = new Vue({
