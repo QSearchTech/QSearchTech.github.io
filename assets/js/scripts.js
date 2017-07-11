e505537058cee4e57c815bd85e660389a4a356f6
Steff Yang <wonderwallfrank@gmail.com>
2017-07-11 18:02:40 +0800

    buggy

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ var data = {
 	current_step: 1,
 	chart_preview: false,
 	chart: {
+		title: '',
 		type: 'bar',
 		x_label: '',
 		x_data: '',
@@ -11,6 +12,9 @@ var data = {
 		y_label_2_percentage: false,
 		y_dataset: [],
 	},
+	filename: '',
+	canvas_content: {},
+	chart_file: null
 };
 
 var vm = new Vue({
@@ -81,7 +85,7 @@ var vm = new Vue({
 		//   return check;
 		// },
 		preview_chart: function () {
-			// vm.chart_preview = true;
+			vm.chart_preview = true;
 			// device handling
 			// var width = window.innerWidth;
 			// if (isMobile()) {
@@ -89,13 +93,12 @@ var vm = new Vue({
 			//     document.getElementById('chart').setAttribute('height', 360);
 			// }
 			// data handling
-			var canvas_content = {};
-			canvas_content.type = vm.chart.type;
-			canvas_content.data = {
+			vm.canvas_content.type = vm.chart.type;
+			vm.canvas_content.data = {
 				labels: vm.seperate_new_line(vm.chart.x_data),
 			};
 
-			canvas_content.data.datasets = vm.chart.y_dataset.map(function(element){
+			vm.canvas_content.data.datasets = vm.chart.y_dataset.map(function(element){
 				console.log(element);
 				var new_element = {};
 				var color = vm.detect_color(element.color);
@@ -115,8 +118,12 @@ var vm = new Vue({
 			});
 
 			// chart options configuring
-			canvas_content.options = {
+			vm.canvas_content.options = {
 				// responsive: false,
+				title: {
+		            display: true,
+		            text: vm.chart.title
+		        },
 				scales: {
 					xAxes: [{
 						scaleLabel: {
@@ -144,9 +151,9 @@ var vm = new Vue({
 
 			// Y Axis handling
 			if (vm.chart.y_label_2 !== '') {
-				canvas_content.options.scales.yAxes = [];
+				vm.canvas_content.options.scales.yAxes = [];
 				if (vm.chart.y_label_1_percentage === true) {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'left',
 						id: "y-axis-1",
 						scaleLabel: {
@@ -161,7 +168,7 @@ var vm = new Vue({
 						}
 					});
 				} else {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'left',
 						id: "y-axis-1",
 						scaleLabel: {
@@ -175,7 +182,7 @@ var vm = new Vue({
 				}
 
 				if (vm.chart.y_label_2_percentage === true) {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'right',
 						id: "y-axis-2",
 						scaleLabel: {
@@ -190,7 +197,7 @@ var vm = new Vue({
 						}
 					});
 				} else {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'right',
 						id: "y-axis-2",
 						scaleLabel: {
@@ -203,9 +210,9 @@ var vm = new Vue({
 					});
 				}
 			} else {
-				canvas_content.options.scales.yAxes = [];
+				vm.canvas_content.options.scales.yAxes = [];
 				if (vm.chart.y_label_1_percentage === true) {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'left',
 						id: "y-axis-1",
 						scaleLabel: {
@@ -220,7 +227,7 @@ var vm = new Vue({
 						}
 					});
 				} else {
-					canvas_content.options.scales.yAxes.unshift({
+					vm.canvas_content.options.scales.yAxes.unshift({
 						position: 'left',
 						id: "y-axis-1",
 						scaleLabel: {
@@ -235,7 +242,36 @@ var vm = new Vue({
 			}
 
 			var ctx = document.getElementById('chart').getContext('2d');
-			var chart = new Chart(ctx,canvas_content);
+			var chart = new Chart(ctx,vm.canvas_content);
+		},
+		render_file: function (text, filename) {
+			var file = new File([text], filename, {type: 'text/plain'});
+
+			// Manually revoke the object URL to avoid memory leaks
+			if (vm.chart_file !== null) {
+				window.URL.revokeObjectURL(chart_file);
+			}
+
+			vm.chart_file = window.URL.createObjectURL(file);
+			return vm.chart_file;
+		},
+ 		download_file: function () {
+ 			console.log('download');
+ 			// mobile chart adjustments
+ 			vm.canvas_content.options.responsive = false;
+ 			vm.canvas_content.options.scales.xAxes[0].ticks.fontSize = isMobile() ? ( width < 360 ? 8 : 10) : 12;
+ 			vm.canvas_content.options.tooltips.titleFontSize = isMobile() ? ( width < 360 ? 12 : 14) : 18;
+          	vm.canvas_content.options.tooltips.bodyFontSize = isMobile() ? ( width < 360 ? 10 : 12) : 16;
+          	vm.canvas_content.options.scales.yAxes.map(function(element){
+          		element.ticks.fontSize = isMobile() ? ( width < 360 ? 8 : 10) : 12;
+          		return element;
+          	});
+
+			var chart_text = 
+				'<!DOCTYPE html><html><head><title></title></head><body><canvas id="myChart" width="300" height="150"></canvas><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script><script type="text/javascript">var width=window.innerWidth;function isMobile(){var check=false; (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))){check=true;}})(navigator.userAgent||navigator.vendor||window.opera); return check;}if (isMobile()){document.getElementById("myChart").setAttribute("width", 300); document.getElementById("myChart").setAttribute("height", 250); console.log(width);}var ctx=document.getElementById("myChart").getContext("2d");var chart=new Chart(ctx,{' + vm.canvas_content.toString() + ');</script></body></html>';
+
+			var link = vm.render_file(chart_text, vm.filename);
+			window.open(link, "_blank");
 		}
 	},
 });
