fe7ece831cdef043a728e8ad2862d0f650f926d5
Steff Yang <wonderwallfrank@gmail.com>
2017-08-25 15:45:15 +0800

    blob download for son

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -75,9 +75,8 @@ var vm = new Vue({
 	        Papa.parse(target_file, config);
 		},
 		create_chord: function(){
-			var json = "data:text/json;charset=utf-8" + encodeURIComponent(vm.chord.json_data);
 			var json_link = document.getElementById('hidden-json-download');
-			json_link.setAttribute('href',json);
+			json_link.href = vm.render_file(vm.chord.json_data,'text/json');
 			json_link.setAttribute('download',vm.chord.json_filename + '.json');
 			json_link.click();
 
@@ -134,7 +133,7 @@ var vm = new Vue({
 				'</script></body></html>';
 
 			var chord_link = document.getElementById('hidden-chord-download');
-			chord_link.href = vm.render_file(chord_text);
+			chord_link.href = vm.render_file(chord_text,'text/plain');
 			chord_link.setAttribute('download',vm.chord.chord_filename + '.html');
 			chord_link.click();
 
@@ -383,7 +382,7 @@ var vm = new Vue({
 				'<!DOCTYPE html><html><head><title></title></head><body><canvas id="myChart" width="900" height="450"></canvas><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script><script type="text/javascript">var width=window.innerWidth;function isMobile(){var check=false;(function(a){if(/(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt(|\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-/i.test(a.substr(0,4))){check=true;}})(navigator.userAgent||navigator.vendor||window.opera);return check;}if(isMobile()){document.getElementById(\"myChart\").setAttribute(\"width\",300);document.getElementById(\"myChart\").setAttribute(\"height\",250);console.log(width);}var ctx=document.getElementById(\"myChart\").getContext(\"2d\");var chart=new Chart(ctx,' + vm.chart_data + ');</script></body></html>';
 
 			var link = document.getElementById('download-chart');
-			link.href = vm.render_file(chart_text);
+			link.href = vm.render_file(chart_text,'text/plain');
 			// window.open(link, "_blank");
 		},
 		preview_table: function() {
@@ -409,11 +408,11 @@ var vm = new Vue({
 					'<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"><style>h5{padding-top:15px}table{color:#333;margin-top:20px;font-size:.8rem}table thead th.bg-purple{background-color:#E2DEEF;border:none}@media (max-width:576px){h5{font-size:1rem}table{font-size:.5rem}.table td,.table th{padding:.5rem}}</style></head><body><div class="container"><div class="row"><div class="col-lg-12 col-md-12 col-sm-12" id="main-content"><h5 class="text-center p-t-20">' + vm.title + '</h5>' + table_content + '</div></div></div><script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script><script src="https://qsearchtech.github.io/archives/common.js"></script></body></html>';
 
 				var link = document.getElementById('download-table');
-				link.href = vm.render_file(table_text);
+				link.href = vm.render_file(table_text,'text/plain');
 			}, 3000);
 		},
-		render_file: function (text) {
-			var file = new Blob([text], {type: 'text/plain'});
+		render_file: function (text,type) {
+			var file = new Blob([text], {type: type});
 
 			// Manually revoke the object URL to avoid memory leaks
 			if (vm.chart_file !== null) {
